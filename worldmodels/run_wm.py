81c607c8f83e8ec84314250223d5aa326f75b96d
salocinrevenge <nicolashecker.nhs@gmail.com>
2026-07-27 16:09:20 -0300

    adiciona conteudo do lewm corrigido para rodar direito

---
+++ worldmodels/run_wm.py
@@ -0,0 +1,7 @@
+"""
+This program is used to run a world model in a specified environment of
+dataset and parameters as defined by its configuration file as a giver .py
+
+Usage: 
+    python run_wm.py <path_to_config_file>
+"""
